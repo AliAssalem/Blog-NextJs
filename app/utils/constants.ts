a4129e974ab3aa777044db19afa65b7749981366
AliAssalem <ali1assaelm@gmail.com>
2026-05-04 18:08:24 +0300

    fix production domain

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,6 +1,6 @@
 export const ARTICLE_PER_PAGE = 6;
 
-const PRODUCTION_DOMAIN = "blog-next-2oec12mjb-ali1assalemgmailcoms-projects.vercel.app";
+const PRODUCTION_DOMAIN = "https://blog-next-2oec12mjb-ali1assalemgmailcoms-projects.vercel.app";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 //const PRODUCTION_DOMAIN = "https://cloud-hosting-project-course.vercel.app";
 
