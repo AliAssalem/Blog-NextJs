aa67185b0cc30a1db617b12bfbba5412d492f749
AliAssalem <ali1assaelm@gmail.com>
2026-05-04 21:05:30 +0300

    fix navbar visibility

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -2,6 +2,7 @@ export const ARTICLE_PER_PAGE = 6;
 
 const PRODUCTION_DOMAIN = "https://blog-next-js-tan.vercel.app";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
-//const PRODUCTION_DOMAIN = "https://cloud-hosting-project-course.vercel.app";
 
-export const DOMAIN = "https://blog-next-js-tan.vercel.app";
+export const DOMAIN = process.env.NODE_ENV === 'production' 
+    ? PRODUCTION_DOMAIN
+    : DEVELOPMENT_DOMAIN;
