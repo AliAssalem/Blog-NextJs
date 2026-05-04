4cb412b5e24263433c1bf8b40b9e613cf6bccefd
AliAssalem <ali1assaelm@gmail.com>
2026-05-04 18:36:17 +0300

    fix domain

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -4,6 +4,4 @@ const PRODUCTION_DOMAIN = "https://blog-next-js-tan.vercel.app";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 //const PRODUCTION_DOMAIN = "https://cloud-hosting-project-course.vercel.app";
 
-export const DOMAIN = process.env.NODE_ENV === 'production' 
-    ? PRODUCTION_DOMAIN
-    : DEVELOPMENT_DOMAIN;
+export const DOMAIN = "https://blog-next-js-tan.vercel.app";
